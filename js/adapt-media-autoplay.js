12fd35b9438ab774e3ce20d0bb47f1aacc4af8d3
Dan Gray <dan@sinensis.co.uk>
2016-04-25 13:58:05 +0100

    Add extra reduced text check

--- js/adapt-media-autoplay.js
+++ js/adapt-media-autoplay.js
@@ -27,7 +27,7 @@ define(function(require) {
         postRender: function() {
             this.setupPlayer();
 
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 this.replaceText(Adapt.audio.textSize);
             }
             // Check if notify is visible
@@ -104,7 +104,7 @@ define(function(require) {
         // Reduced text
         replaceText: function(value) {
             // If enabled
-            if (this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
+            if (Adapt.config.get('_audio') && Adapt.config.get('_audio')._isReducedTextEnabled && this.model.get('_reducedText') && this.model.get('_reducedText')._isEnabled) {
                 if(value == 0) {
                     this.$('.component-title-inner').html(this.model.get('displayTitle')).a11y_text();
                     this.$('.component-body-inner').html(this.model.get('body')).a11y_text();
